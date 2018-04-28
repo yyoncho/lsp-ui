f8d8c877e9b79db9956c38fd11f2bcc47c3dcb1b
kyoncho <yyoncho@gmail.com>
2018-04-28 07:36:06 +0300

    Flycheck

--- lsp-ui-flycheck.el
+++ lsp-ui-flycheck.el
@@ -173,21 +173,23 @@ CALLBACK is the status callback passed by Flycheck."
   ;; Turn all errors from lsp--diagnostics for the current buffer into
   ;; flycheck-error objects and pass them immediately to the callback
   (let ((errors))
-    (dolist (diag (or (gethash buffer-file-name lsp--diagnostics)
-                      (gethash (file-truename buffer-file-name) lsp--diagnostics)))
-      (push (flycheck-error-new
-             :buffer (current-buffer)
-             :checker checker
-             :filename buffer-file-name
-             :line (1+ (lsp-diagnostic-line diag))
-             :column (1+ (lsp-diagnostic-column diag))
-             :message (lsp-diagnostic-message diag)
-             :level (pcase (lsp-diagnostic-severity diag)
-                      (1 'error)
-                      (2 'warning)
-                      (_ 'info))
-             :id (lsp-diagnostic-code diag))
-            errors))
+    (maphash
+     (lambda (file-name diagnostics)
+       (dolist (diag diagnostics)
+         (push (flycheck-error-new
+                :buffer (get-file-buffer file-name)
+                :checker checker
+                :filename file-name
+                :line (1+ (lsp-diagnostic-line diag))
+                :column (1+ (lsp-diagnostic-column diag))
+                :message (lsp-diagnostic-message diag)
+                :level (pcase (lsp-diagnostic-severity diag)
+                         (1 'error)
+                         (2 'warning)
+                         (_ 'info))
+                :id (lsp-diagnostic-code diag))
+               errors)))
+     lsp--diagnostics)
     (funcall callback 'finished errors)))
 
 (flycheck-define-generic-checker 'lsp-ui
